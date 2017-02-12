0e07a5f6ffaedebfd4fa037899fc3b9160e8e301
Felix Haschke <haschke.felix@gmail.com>
2017-02-12 08:57:30 +0100

    class NameTag

---
+++ nametag.cpp
@@ -0,0 +1,50 @@
+#include "nametag.h"
+
+NameTag::NameTag(byte num)
+   : MatrixDisplay(num)
+   , start_col_(0), shift_mode_(AUTO_SHIFT), shift_count_(0), shift_speed_(10)
+{
+}
+
+void NameTag::update() {
+	if (shift_mode_ & SHIFT) {
+		if (++shift_count_ == shift_speed_) {
+			shift_count_ = 0;
+			shift();
+		}
+	}
+}
+
+void NameTag::shift()
+{
+	--start_col_;
+	if (setString(text_, start_col_) == 0)
+		start_col_ = 7;
+}
+
+void NameTag::setText(const char* text)  //sets a new string text
+{
+	text_ = text;
+
+	// need to recompute actual shift mode?
+	if (shift_mode_ & AUTO_SHIFT)
+		setShiftMode(AUTO_SHIFT);
+}
+
+void NameTag::setShiftMode(NameTag::ShiftMode mode) //sets the shiftmode
+{
+	shift_mode_ = mode;
+	if (shift_mode_ & AUTO_SHIFT) {        // if the shift mode is auto,
+		bool shift = width(text_) > cols(); // determined if shifting is needed
+		bitWrite(shift_mode_, 0, shift); // set the second bit of shift_mode_ to
+		                                 // previously calculated state
+	}
+	if (shift_mode_ & SHIFT) // when shifting, start is last column
+		start_col_ = cols()-1;
+	else // otherwise, start in first column + setString once
+		setString(text_, start_col_ = 0);
+}
+
+void NameTag::setShiftSpeed(int speed) {  //sets the shift speed
+	shift_speed_ = speed;
+}
---
+++ nametag.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "matrixdisplay.h"
+
+class NameTag : public MatrixDisplay
+{
+public:
+	enum ShiftMode { NO_SHIFT = 0,
+		              SHIFT = 1,
+		              AUTO_SHIFT = 2
+	               };
+
+	NameTag(byte num=1);
+	void setText(const char* text);
+	void update();
+
+	void shift();
+	void setShiftMode(ShiftMode mode);
+	void setShiftSpeed(int speed);
+
+private:
+	const char* text_;
+	char start_col_;
+	byte shift_mode_;
+	byte shift_count_ = 0;
+	byte shift_speed_ = 10;
+};
