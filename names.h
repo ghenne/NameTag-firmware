22807d1fad9517867408d0f4dddbc32c652e1a4b
Felix Haschke <haschke.felix@gmail.com>
2018-01-29 17:27:53 +0100

    fixup names and spelling in nametag_sm

--- names.h
+++ names.h
@@ -1,11 +1,13 @@
 #pragma once
 
 /* Umlaute:
-Ä ""\x1d""
-Ö ""\x1e""
-Ü ""\x1f""
+ * ä ""\x1b""
+ * ö ""\x1c""
+ * Ä ""\x1d""
+ * Ö ""\x1e""
+ * Ü ""\x1f""
 */
-#define NAME1 "Friedrich"
+#define NAME1 "Tekkietorium - Technik entsteht, Bit für Bit"
 #define NAME2 ""
 #define NAME3 ""
 #define NAME4 ""
@@ -14,4 +16,8 @@
 #define NAME7 ""
 #define NAME8 ""
 
+#if 1
 #define default_language DEUTSCH
+#else
+#define default_language ENGLISH
+#endif
